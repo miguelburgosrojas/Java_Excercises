36c451aeef415838577f9c91a653ef73e8ae021b
Miguel Burgos <miguel.burgos@bpsconsultores.com>
2020-11-10 16:37:48 -0500

    HexadecimalToDecimal

--- src/HexadecimalToDecimal.java
+++ src/HexadecimalToDecimal.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 
 public class HexadecimalToDecimal {
+
+    public static int hex_to_decimal(String s)
+    {
+        String digits = "0123456789ABCDEF";
+        s = s.toUpperCase();
+        int val =0;
+        for(int i=0; i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val + d;
+        }
+        return val;
+    }
+
     public static void main(String[] args) {
         System.out.println("..................******.................................................................");
         System.out.println(".................*$$$$$V.................................................................");
@@ -22,5 +37,14 @@ public class HexadecimalToDecimal {
         System.out.println("..............................:N$$$$V....................................................");
         System.out.println("..............................V$$$$N*....................................................");
         System.out.println("..............................VIIIIV.....................................................");
+
+        String hexdec_num;
+        int dec_num;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero hexadecimal : ");
+        hexdec_num = scan.nextLine();
+        dec_num = hex_to_decimal(hexdec_num);
+        System.out.print("El numero en decimal es : " + dec_num);
     }
 }
