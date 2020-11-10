fa6e945afe2f530cbbdc5bb28d76465c26c2109b
Miguel Burgos <miguel.burgos@bpsconsultores.com>
2020-11-10 17:41:38 -0500

    HexadecimalToBinary

---
+++ src/HexadecimalToBinary.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class HexadecimalToBinary {
+    public  static int hex_to_decimal(String s)
+    {
+        String digits = "0123456789ABCDEF";
+        s = s.toUpperCase();
+        int val = 0;
+        for (int i=0; i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val+d;
+        }
+        return val;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("..................******.................................................................");
+        System.out.println(".................*$$$$$V.................................................................");
+        System.out.println(".................V$$$$N:.................................................................");
+        System.out.println("................:N$$$$V..................................................................");
+        System.out.println("................V$$$$N::******.......::::::..*****:........:********::...................");
+        System.out.println("...............:N$$$$MFN$$$$$$M*....*$$$$$VVN$$$$$NV:....*FN$$$$$$$$$NF..................");
+        System.out.println("...............V$$$$$$$$N$$$$$$N*..:M$$$$$$$$N$$$$$$M:.:M$$$$$$N$$$$$$V..................");
+        System.out.println("...............N$$$$NF*:::*N$$$$F..*$$$$$$MV*::I$$$$$*:M$$$$V::::::***:..................");
+        System.out.println("..............*$$$$$*......F$$$$I..I$$$$M*.....*$$$$$**$$$$$V*:..........................");
+        System.out.println("..............V$$$$I......:M$$$$V.*$$$$$V......V$$$$$*:N$$$$$$NMFV*:.....................");
+        System.out.println(".............*N$$$$*......*$$$$$*.I$$$$M:.....:M$$$$M:.:*FN$$$$$$$$N*....................");
+        System.out.println(".............V$$$$I......:M$$$$F.*$$$$$V.....:F$$$$$*......:*VI$$$$$M:...................");
+        System.out.println("............:N$$$$*....:*N$$$$M:.F$$$$N:....:I$$$$$V.::.......*N$$$$F....................");
+        System.out.println("............V$$$$$NMIIMN$$$$$M:.*N$$$$$NMMIMN$$$$N*.:N$NMMIIIM$$$$$M*....................");
+        System.out.println("...........:M$$$$$$$$$$$$$$M*:..V$$$$$$$$$$$$$$MV:..V$$$$$$$$$$$$MV:.....................");
+        System.out.println("..............:*****VVV***.....*N$$$$V:***VV**:......::***VVV***:........................");
+        System.out.println("...............................V$$$$N:...................................................");
+        System.out.println("..............................:N$$$$V....................................................");
+        System.out.println("..............................V$$$$N*....................................................");
+        System.out.println("..............................VIIIIV.....................................................");
+
+
+        String hexdec_num;
+        int dec_num, i=1,j;
+        int bin_num[] = new int[100];
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Ingrese numero hexadecimal : ");
+        hexdec_num = scan.nextLine();
+
+        dec_num = hex_to_decimal(hexdec_num);
+
+        while(dec_num !=0)
+        {
+            bin_num[i++] = dec_num%2;
+            dec_num = dec_num/2;
+        }
+        System.out.println("El numero binario es : ");
+        for(j=i-1;j>0;j--)
+        {
+            System.out.print(bin_num[j]);
+        }
+
+        System.out.print("\n");
+
+
+    }
+}
