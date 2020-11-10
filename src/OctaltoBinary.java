d5774797930e3103009edb7d121dc58ba66e540d
Miguel Burgos <miguel.burgos@bpsconsultores.com>
2020-11-10 11:43:11 -0500

    OctaltoBinary class

--- src/OctaltoBinary.java
+++ src/OctaltoBinary.java
@@ -4,6 +4,10 @@ public class OctaltoBinary {
     public static void main(String[] args)
     {
         System.out.println("Hola Mundo");
+        Scanner in = new Scanner(System.in);
+        int[] octal_numvalues = {0,1,10,11,100,101,110,111};
+        long octal_num, temoctal_num, binary_num, place;
+
     }
 
 }
