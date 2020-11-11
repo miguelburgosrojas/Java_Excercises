96fa5058f934f936fb5f4dd4ba637baf62b20613
Miguel Burgos <miguel.burgos@bpsconsultores.com>
2020-11-11 08:53:52 -0500

    JavaInfoPC

---
+++ src/JavaInfoPC.java
@@ -0,0 +1,32 @@
+public class JavaInfoPC {
+    public static void main(String [] args)
+    {
+        System.out.println("..................******.................................................................");
+        System.out.println(".................*$$$$$V.................................................................");
+        System.out.println(".................V$$$$N:.................................................................");
+        System.out.println("................:N$$$$V..................................................................");
+        System.out.println("................V$$$$N::******.......::::::..*****:........:********::...................");
+        System.out.println("...............:N$$$$MFN$$$$$$M*....*$$$$$VVN$$$$$NV:....*FN$$$$$$$$$NF..................");
+        System.out.println("...............V$$$$$$$$N$$$$$$N*..:M$$$$$$$$N$$$$$$M:.:M$$$$$$N$$$$$$V..................");
+        System.out.println("...............N$$$$NF*:::*N$$$$F..*$$$$$$MV*::I$$$$$*:M$$$$V::::::***:..................");
+        System.out.println("..............*$$$$$*......F$$$$I..I$$$$M*.....*$$$$$**$$$$$V*:..........................");
+        System.out.println("..............V$$$$I......:M$$$$V.*$$$$$V......V$$$$$*:N$$$$$$NMFV*:.....................");
+        System.out.println(".............*N$$$$*......*$$$$$*.I$$$$M:.....:M$$$$M:.:*FN$$$$$$$$N*....................");
+        System.out.println(".............V$$$$I......:M$$$$F.*$$$$$V.....:F$$$$$*......:*VI$$$$$M:...................");
+        System.out.println("............:N$$$$*....:*N$$$$M:.F$$$$N:....:I$$$$$V.::.......*N$$$$F....................");
+        System.out.println("............V$$$$$NMIIMN$$$$$M:.*N$$$$$NMMIMN$$$$N*.:N$NMMIIIM$$$$$M*....................");
+        System.out.println("...........:M$$$$$$$$$$$$$$M*:..V$$$$$$$$$$$$$$MV:..V$$$$$$$$$$$$MV:.....................");
+        System.out.println("..............:*****VVV***.....*N$$$$V:***VV**:......::***VVV***:........................");
+        System.out.println("...............................V$$$$N:...................................................");
+        System.out.println("..............................:N$$$$V....................................................");
+        System.out.println("..............................V$$$$N*....................................................");
+        System.out.println("..............................VIIIIV.....................................................");
+
+        System.out.println("\nJava version: " + System.getProperty("java.version"));
+       System.out.println("Java Runtime version : " + System.getProperty("java.runtime.version"));
+       System.out.println("Java Home : " + System.getProperty("java.home"));
+       System.out.println("Java Vendor : " + System.getProperty("java.vendor"));
+       System.out.println("Java Vendor URL : " + System.getProperty("java.vendor.url"));
+       System.out.println("Java Class Path : " + System.getProperty("java.class.path") + "\n");
+    }
+}
